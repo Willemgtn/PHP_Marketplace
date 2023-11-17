4319570dda53f5df554503567fe2996a721d911f
Willemgtn <a.w.cerqueira@hotmail.com>
2023-11-17 18:23:14 -0300

    Login page

---
+++ pages/login.php
@@ -0,0 +1,48 @@
+<?php 
+    if(isset($_POST['acao'])){
+      $login = strip_tags($_POST['login']);
+      $senha = strip_tags($_POST['senha']);
+
+      $sql = MySql::getConn()->prepare("SELECT * FROM usuarios WHERE login = ? and senha = ?");
+      $sql -> execute(array($login, $senha));
+
+      if($sql->rowCount() == 1){
+        // logged in
+        $_SESSION['login'] = $login;
+      } else {
+        // Failed login attempt
+        die("username or password incorrect");
+      }
+    };
+    if(isset($_SESSION['login'])){
+      echo '<script>location.href="/"</script>';
+    }
+    
+    if(!isset($_SESSION['login'])){
+?>
+<h1>Login page</h1>
+<div class="container">
+  <form method="post">
+    <img class="mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input name="login" type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Login</label>
+    </div>
+    <br>
+    <div class="form-floating">
+      <input name="senha" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button name='acao' class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+  </form>
+</div>
+<?php }; ?>
